dc0722e04fb3f441702f7e8d984f869cc02c7dad
Owen Bradley <opbradley@ucdavis.edu>
2023-09-11 21:34:40 -0700

    Updating docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -35,9 +34,9 @@ release = "0.1.0"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = [
-    "sphinx_rtd_theme"
- ]
+# extensions = [
+#    "sphinx_rtd_theme"
+# ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -55,7 +54,8 @@ exclude_patterns = []
 #
 
 # html_theme = "alabaster"
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
+html_theme = "nature"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
