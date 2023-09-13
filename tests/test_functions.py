6a39fde7f656880e71a9d93bc4ceeb7d02b1f3d4
Owen Bradley <opbradley@ucdavis.edu>
2023-09-12 21:31:27 -0700

    Added tests for lattice neighbors

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -20,7 +20,35 @@ def test_ternary_pad():
     assert symmetry_functions.ternary_pad(30, 12) == '000000001010'
 
 
+def test_x_neighbors():
+
+    x_neighbors = symmetry_functions.get_neighbors(Lx=3, Ly=3, bond='x')
+    x_pairs = [(0, 3), (2, 5), (1, 4), (6, 9), (7, 10), (8, 11), (12, 15), (13, 16), (14, 17)]
+
+    assert set(x_neighbors) == set(x_pairs)
+
+
+def test_y_neighbors():
+
+    y_neighbors = symmetry_functions.get_neighbors(Lx=3, Ly=3, bond='y')
+    y_pairs = [(3, 6), (4, 7), (5, 8), (9, 12), (10, 13), (11, 14), (0, 15), (1, 16), (2, 17)]
+
+    assert set(y_neighbors) == set(y_pairs)
+
+
+def test_z_neighbors():
+
+    z_neighbors = symmetry_functions.get_neighbors(Lx=3, Ly=3, bond='z')
+    z_pairs = [(1, 3), (2, 4), (0, 5), (7, 9), (8, 10), (6, 11), (13, 15), (14, 16), (12, 17)]
+
+    assert set(z_neighbors) == set(z_pairs)
+
+
 test_tern_to_base10()
 test_ternary()
 test_tern_to_base10()
+test_x_neighbors()
+test_y_neighbors()
+test_z_neighbors()
+
 
