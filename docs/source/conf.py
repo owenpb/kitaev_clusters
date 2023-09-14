a66bb2627714c71cc8e117a08d68c64c3921082c
Owen Bradley <opbradley@ucdavis.edu>
2023-09-13 20:01:55 -0700

    Updated autodoc imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,8 @@ extensions = [
     "sphinx.ext.napoleon"
 ]
 
+autodoc_mock_imports = ["numpy", "scipy"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
