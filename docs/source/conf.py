5ef7dbcf380b63a1f2613a498dd1a74154cc4053
Owen Bradley <opbradley@ucdavis.edu>
2024-03-16 19:57:54 +0000

    Updated docs configuration file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = "kitaev_clusters"
-copyright = "2023, Owen Bradley"
+copyright = "2024, Owen Bradley"
 author = "Owen Bradley"
 
 # The full version, including alpha/beta/rc tags
